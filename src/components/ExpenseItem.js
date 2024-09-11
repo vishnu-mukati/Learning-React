88cebb2e4fce22100afaa7764500c455a93a4947
vishnu-mukati <mukativishnu1232@gmail.com>
2024-09-11 16:53:06 +0530

    javascript inside JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,21 @@
 import "./ExpenseITem.css"
 
 function ExpenseItem(){
+    const expenseDate = new Date(2023,1,14).toISOString();
+    const expenseTitle = "insurance"
+    const expenseAmount = 50;
+    const expenseLocation = "Delhi"
     return (
         <div className ="expense-item">
             <div>
-                <div>15 August 2023</div>
-            </div>
-            <div className ="expense-item__description">
-                <h2>Book</h2>
-                <div className ="expense-item__price">$10</div>
+                <div>{expenseDate}</div>
             </div>
             <div className = "expense-item__location">
-                <h2>Delhi</h2>
+                <h2>{expenseLocation}</h2>
+            </div>
+            <div className ="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className ="expense-item__price">{expenseAmount}</div>
             </div>
         </div>
     );

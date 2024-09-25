1c45d4fcb090a598ce39f79d2e79c52cad9b2314
vishnu-mukati <mukativishnu1232@gmail.com>
2024-09-25 14:57:19 +0530

    giving calendar like look by the methoid of get month and date and year

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,36 @@
 import "./ExpenseITem.css"
 
-function ExpenseItem(props){
+function ExpenseItem(props) {
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+    const month = months[props.date.getMonth()];
+    const year = props.date.getFullYear();
+    const date = props.date.getDate();
     return (
-        <div className ="expense-item">
+        <div className="expense-item">
             <div>
-                <div>{props.date.toISOString()}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{date}</div>
             </div>
-            <div className = "expense-item__location">
+            <div className="expense-item__location">
                 <h2>{props.location}</h2>
             </div>
-            <div className ="expense-item__description">
+            <div className="expense-item__description">
                 <h2>{props.title}</h2>
-                <div className ="expense-item__price">{props.amount}</div>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );

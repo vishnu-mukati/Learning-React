fe1e5a99427262737324f88ffa1c5a188522848e
vishnu-mukati <mukativishnu1232@gmail.com>
2024-09-26 12:55:08 +0530

    spliting the  component and

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,31 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+
+    const month = months[props.CalendarDate.getMonth()];    
+    const year = props.CalendarDate.getFullYear();
+    const date = props.CalendarDate.getDate();
+    return (
+        <div className ="expense-date">
+            <div className ="expense-date__month">{month}</div>
+            <div className ="expense-date__year">{year}</div>
+            <div className ="expense-date__day">{date}</div>
+        </div>
+    );
+}
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,30 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseITem.css"
 
 function ExpenseItem(props) {
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    ]
-    const month = months[props.date.getMonth()];
-    const year = props.date.getFullYear();
-    const date = props.date.getDate();
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{date}</div>
-            </div>
+            <ExpenseDate CalendarDate = {props.date}></ExpenseDate>
             <div className="expense-item__location">
                 <h2>{props.location}</h2>
             </div>

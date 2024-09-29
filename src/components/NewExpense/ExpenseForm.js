f716d30255e7b014c2576a680dbc679e19d738ea
vishnu-mukati <mukativishnu1232@gmail.com>
2024-09-29 10:40:16 +0530

    submit the form by store the data in obj and provide the user input as a value attribute to the input and used it to reset the value after form submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,75 +3,52 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
 
-    // this is multiple states
+    const [enteredtitle, setenteredTitle] = useState('');
+    const [enteredamount, setenteredAmount] = useState('');
+    const [entereddate, setenteredDate] = useState('');
 
-    // const [enteredtitle, setenteredTitle] = useState('');
-    // const [enteredamount, setenteredAmount] = useState('');
-    // const [entereddate, setenteredDate] = useState('');
-
-    // this is the single state example
-    const [userInput,setUserInput] = useState({
-        enteredtitle : "",
-        enteredamount : "",
-        entereddate : "",
-    })
 
     const titleChangeHandler = (event) => {
 
-        // this is the multiple state example
-        // setenteredTitle(event.target.value);
-
-        // this is the exaple of single state
-        setUserInput((prevState)=>{
-            return {
-                ...prevState,
-                enteredtitle : event.terget.value,
-            }
-        })
-
+        setenteredTitle(event.target.value);
     }
 
     const amountChangeHandler =(event) =>{
 
-        // this is the multiple state example
-        // setenteredAmount(event.target.value);
-
-         // this is the exaple of single state
-         setUserInput((prevState)=>{
-            return {
-                ...prevState,
-                enteredamount : event.terget.value,
-            }
-        })
+        setenteredAmount(event.target.value);
     }
 
     const dateChangeHandler = (event) =>{
 
-        // this is the mmultiple state example
-        // setenteredDate(event.target.value);
+        setenteredDate(event.target.value);
+        }
 
-         // this is the exaple of single state
-         setUserInput((prevState)=>{
-            return {
-                ...prevState,
-                entereddate : event.terget.value,
-            }
-        })
-    }
+    const formSubmitHandler = (event)=>{
+        event.preventDefault();
+        const ExpenseData = {
+             title : enteredtitle,
+             amount: enteredamount,
+             date : new Date(entereddate),
+        }
+        console.log(ExpenseData);
+        setenteredTitle("");
+        setenteredAmount("");
+        setenteredDate("");
+    }    
 
-    return <form>
+    return <form onSubmit={formSubmitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label htmlFor="title">Title</label>
-                <input type="text" onChange={titleChangeHandler} />
+                <input type="text" value={enteredtitle} onChange={titleChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label htmlFor="amount">Amount</label>
-                <input type="number" onChange={amountChangeHandler}/>
+                <input type="number" value={enteredamount} onChange={amountChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label htmlFor="date">Date</label>
-                <input type="date" onChange={dateChangeHandler} min="2023-01-01" max="2024-12-31" />
+                <input type="date" value={entereddate} onChange={dateChangeHandler} min="2023-01-01" max="2024-12-31" />
             </div>
             <div className="new-expense__actions">
                 <button type="submit">Add Expense</button>
